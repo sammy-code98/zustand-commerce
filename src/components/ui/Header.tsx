abd94c506d5eaab120e82f0c0f2a050dac0f068d
sammy-code98 <sammysmart98@yahoo.com>
2023-10-18 06:47:02 +0100

    store update

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FiShoppingCart } from "react-icons/fi"
 import { CartButton } from '../../utils/types';
-
+import { useCartStore } from '@/store/useCartStore';
+import useFromStore from '@/hooks/useFromStore';
 
 
 export default function Header({ onCartIcon }: CartButton) {
+    const cart = useFromStore(useCartStore, state => state.cart)
     return (
         <header className='bg-blue-600 text-white py-4 flex items-center justify-between h-16 sticky top-0 z-10'>
             <nav className='container mx-auto md:w-10/12 px-4 flex justify-between'>
@@ -12,7 +14,7 @@ export default function Header({ onCartIcon }: CartButton) {
                 <div className='relative'>
                     <button type='button' className='text-white text-xl flex items-center' onClick={onCartIcon}>
                         <FiShoppingCart />
-                        <div className='text-white rounded-full bg-blue-300 w-5 h-5 text-sm -ml-1'>2</div>
+                        <div className='text-white rounded-full bg-blue-300 w-5 h-5 text-sm -ml-1'>{cart?.length}</div>
                     </button>
                 </div>
 
