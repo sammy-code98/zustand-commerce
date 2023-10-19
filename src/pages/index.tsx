da354d11d2cd9ba55f29ed232b89c55bb2914f63
sammy-code98 <sammysmart98@yahoo.com>
2023-10-19 05:31:59 +0100

    fix:resolve issue with dummy json images iin next config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Inter } from 'next/font/google'
 import Header from '@/components/ui/Header'
 import Drawer from '@/components/ui/Drawer'
 import Cart from '@/components/cart/Cart'
+import ProductList from '@/components/products/ProductList'
+import { useProductStore } from '@/store/useProductStore'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+
+  const { products, isLoading, error, fetchData } = useProductStore()
+
+  useEffect(() => {
+    // call the fetchdata function
+    fetchData()
+  }, [fetchData])
 
   const handleCartIcon = () => {
     setIsDrawerOpen(!isDrawerOpen)
@@ -18,10 +28,8 @@ export default function Home() {
       <Drawer isOpen={isDrawerOpen} onCartIcon={handleCartIcon}>
         <Cart />
       </Drawer>
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div>hhehe</div>
+      <main className={`container mx-auto md:w-10/12 py-8 px-4 ${inter.className}`} >
+        {isLoading ? <div className='text-center text-lg'>Loading...</div> : <ProductList products={products} />}
     </main>
     </>
   )
